f3591c180719fb145a7597a6a6b8a859632532c2
Farhad Khan <110168481+farhadmohmand66@users.noreply.github.com>
2022-08-16 12:41:26 +0500

    Update settings.py

--- saDeploy/settings.py
+++ saDeploy/settings.py
@@ -1,7 +1,9 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
