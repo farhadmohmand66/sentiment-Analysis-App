f7f887722f94e2c62e98796c715a7f44fa3b591c
Farhad Khan <110168481+farhadmohmand66@users.noreply.github.com>
2022-08-16 17:52:37 +0500

    Update urls.py

--- saDeploy/urls.py
+++ saDeploy/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
     path('', include('saApp.urls')),
     path('admin/', admin.site.urls),
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
