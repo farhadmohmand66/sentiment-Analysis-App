ea2c6041f24a723dcd3efdd7b7d869493ff31294
Farhad Khan <110168481+farhadmohmand66@users.noreply.github.com>
2022-08-16 19:17:18 +0500

    Update settings.py

--- saDeploy/settings.py
+++ saDeploy/settings.py
@@ -13,7 +13,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = 'django-insecure-w^3dc7m&^0j5-=a^-26=0kpr5)pfx)4qbdiul06+z)4h8np(^c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','sentimentanalysispashto.herokuapp.com']
 
