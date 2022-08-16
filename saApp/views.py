278690165020cb26d4fbeae2bb2fc6914983c70a
Farhad Khan <farhadmohmand66@gmail.com>
2022-08-16 11:30:52 +0500

    first commit

---
+++ saApp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+import numpy as np
+
+from joblib import load
+model = load('./savedModels/nbModelPred.joblib')
+vect = load('./savedModels/vect.joblib')
+from nltk.corpus import stopwords # to remove the stopwrods
+from nltk.stem.porter import PorterStemmer # steam to root word every
+import re
+steamer = PorterStemmer()
+
+def predictor(request):
+    if request.method == 'POST':
+        opinion = request.POST['opinion']
+        opinion = re.sub('[^a-zA-Z]', ' ', opinion)
+        opinion = opinion.lower()
+        opinion = opinion.split()
+        print(type(opinion), opinion, ":this is 2")
+        steamer = PorterStemmer()
+        stopwordAll = stopwords.words('english')
+        stopwordAll.remove('not')
+        opinion = [steamer.stem(word) for word in opinion if not word in set(stopwordAll)]
+        opinion = ' '.join(opinion)
+        corpusNew = [opinion]
+        y_pred = model.predict(vect.transform(corpusNew))
+        if y_pred == 0:
+            y_pred = 'Negative'
+        elif y_pred == 1:
+            y_pred = 'Positive'
+        return render(request, 'main.html', {'result' : y_pred})
+    return render(request, 'main.html')
+
