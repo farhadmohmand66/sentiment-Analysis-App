b725937ee09c78fa53941b50a11d0766ef42219e
Farhad Khan <110168481+farhadmohmand66@users.noreply.github.com>
2022-08-17 08:31:28 +0500

    Update settings.py

--- saDeploy/settings.py
+++ saDeploy/settings.py
@@ -3,7 +3,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'saApp/static')
 
 
 # Quick-start development settings - unsuitable for production
